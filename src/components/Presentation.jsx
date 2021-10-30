43eb509a5140609cec53d59c87031cee33fc91bc
Lucas Nonato <lucas.nonato1@hotmail.com>
2021-10-30 18:29:35 -0300

    FIX: Modificado apresentação

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,23 +1,25 @@
 /* eslint-disable react/prefer-stateless-function */
-import React from 'react';
-import ImgPresentation from '../img/Imagem lucas presetation.png';
-import '../styles/Presentation.css';
+import React from "react";
+import ImgPresentation from "../img/Imagem lucas presetation.png";
+import "../styles/Presentation.css";
 
 class Presentation extends React.Component {
   render() {
     return (
       <div className="presentation">
-        <div className="presentation-img-container">
-          <img className="presentation-img" src={ImgPresentation} alt="Imagem de Lucas Nonato" />
-        </div>
-        <div className="presentation-container">
-          <h1 className="presentation-text">Eu sou Lucas Nonato</h1>
-          <h1 className="presentation-text">
-            Desenvolvedor Web <br />
-            <span className="presentation-text-frontend">Full-Stack</span>
-          </h1>
-          <h1 className="presentation-text">UX Designer</h1>
-        </div>
+        <section className="presentation-container">
+          <div className="presentation-img-container">
+            <img className="presentation-img" src={ImgPresentation} alt="Foto de Lucas Nonato" />
+          </div>
+          <div className="presentation-text-container">
+            <h1>Eu sou Lucas Nonato</h1>
+            <h1>
+              Desenvolvedor Web <br />
+              <span className="presentation-span">Full-Stack</span>
+            </h1>
+            <h1>UX Designer</h1>
+          </div>
+        </section>
       </div>
     );
   }
